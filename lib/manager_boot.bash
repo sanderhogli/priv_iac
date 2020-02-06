24602620ec0cc35354f08b395e67438f794bba8a
Erik Hjelmås <erik.hjelmas@ntnu.no>
2020-02-06 11:07:56 +0100

    tempfix

--- lib/manager_boot.bash
+++ lib/manager_boot.bash
@@ -8,16 +8,16 @@ apt-get -o "Dpkg::Options::=--force-confold" dist-upgrade -y --force-yes
 # install Puppet and make sure its services are stopped
 tempdeb=$(mktemp /tmp/debpackage.XXXXXXXXXXXXXXXXXX) || exit 1
 
-i=0;ret=1;
-while [ "$i" -lt "5" ] && [ "$ret" ]; do
+#i=0;ret=1;
+#while [ "$i" -lt "5" ] && [ "$ret" ]; do
   wget -O "$tempdeb" https://apt.puppetlabs.com/puppet6-release-bionic.deb
-  ret="$?"
-  sleep 10
-  let $i++
-done
-if [ "$ret" ]; then # All attempts to download file failed, instruct clound-init to restart and try again
-  exit 1003
-fi
+#  ret="$?"
+#  sleep 10
+#  let $i++
+#done
+#if [ "$ret" ]; then # All attempts to download file failed, instruct clound-init to restart and try again
+#  exit 1003
+#fi
 
  
 dpkg -i "$tempdeb"
