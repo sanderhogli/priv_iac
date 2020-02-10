41b1ee6ac0038d5202ec129cd4d525c2e837d5ca
Jannis Schaefer <5071400-JannisSchaefer@users.noreply.gitlab.com>
2020-02-10 14:55:07 +0100

    Update: Removed updating all hosts before installing puppet, should speed up deployment time

--- lib/windows_boot.ps1
+++ lib/windows_boot.ps1
@@ -10,29 +10,22 @@ if ( $env:computername -ne $shortname ) {
             # https://cloudbase-init.readthedocs.io/en/latest/tutorial.html#file-execution
 }
 #
-# Install all updates (and reboot as much as needed) before installing 
-# Puppet (fixes the problem with installing the Puppet service)
+# Install Puppet
 #
 Set-ExecutionPolicy RemoteSigned -Force
 Install-PackageProvider nuget -Force
-Install-Module PSWindowsUpdate -Force
-if ( (Get-WUList | Measure-Object).Count -gt 0) {
-  Get-WUInstall -Install -AcceptAll -IgnoreReboot
-  exit 1003
-} else {
-  $puppet_agent_msi_url = "https://downloads.puppetlabs.com/windows/puppet/puppet-agent-x64-latest.msi"
-  $puppet_agent_msi_path = Join-Path $ENV:TEMP puppet_agent.msi
-  $ErrorActionPreference = "Stop"
-  $wc = New-Object net.webclient
-  $wc.Downloadfile($puppet_agent_msi_url, $puppet_agent_msi_path)
-  cmd /c start /wait msiexec /qn /i $puppet_agent_msi_path /l*v puppet_agent_msi_log.txt PUPPET_MASTER_SERVER=manager.node.consul
-  del $puppet_agent_msi_path
-  Set-DnsClientGlobalSetting -SuffixSearchList @("node.consul")
-  Get-NetAdapter | Set-DnsClient -ConnectionSpecificSuffix "node.consul"
-  Add-Content "$env:windir\System32\drivers\etc\hosts" "$(& "C:\Program Files\Puppet Labs\Puppet\bin\facter.bat" networking.ip) $(hostname).node.consul $(hostname)"
-  Add-Content "$env:windir\System32\drivers\etc\hosts" "manager_ip_address manager.node.consul"
-  & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" config set server manager.node.consul --section main
-  & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" config set runinterval 300 --section main
-  & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" agent -t
-  & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" resource service puppet ensure=running enable=true
-}
+$puppet_agent_msi_url = "https://downloads.puppetlabs.com/windows/puppet/puppet-agent-x64-latest.msi"
+$puppet_agent_msi_path = Join-Path $ENV:TEMP puppet_agent.msi
+$ErrorActionPreference = "Stop"
+$wc = New-Object net.webclient
+$wc.Downloadfile($puppet_agent_msi_url, $puppet_agent_msi_path)
+cmd /c start /wait msiexec /qn /i $puppet_agent_msi_path /l*v puppet_agent_msi_log.txt PUPPET_MASTER_SERVER=manager.node.consul
+del $puppet_agent_msi_path
+Set-DnsClientGlobalSetting -SuffixSearchList @("node.consul")
+Get-NetAdapter | Set-DnsClient -ConnectionSpecificSuffix "node.consul"
+Add-Content "$env:windir\System32\drivers\etc\hosts" "$(& "C:\Program Files\Puppet Labs\Puppet\bin\facter.bat" networking.ip) $(hostname).node.consul $(hostname)"
+Add-Content "$env:windir\System32\drivers\etc\hosts" "manager_ip_address manager.node.consul"
+& "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" config set server manager.node.consul --section main
+& "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" config set runinterval 300 --section main
+& "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" agent -t
+& "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" resource service puppet ensure=running enable=true
