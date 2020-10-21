469ec8785f4e730d3502a3a1f5a32d6e3fb2545c
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-21 20:38:33 +0200

    Update ubuntu_boot.bash

--- lib/ubuntu_boot.bash
+++ lib/ubuntu_boot.bash
@@ -26,3 +26,6 @@ echo "manager_ip_address manager.node.consul manager" >> /etc/hosts
 /opt/puppetlabs/bin/puppet config set server manager.node.consul --section main
 /opt/puppetlabs/bin/puppet config set runinterval 300 --section main
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+/opt/puppetlabs/bin/puppet agent -t
+/opt/puppetlabs/bin/puppet agent -t
+
